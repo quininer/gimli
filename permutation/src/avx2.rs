6e3428e06ef6432931c8a7e28b4dce52ed82f1ff
quininer <quininer@live.com>
2019-07-31 00:41:53 +0800

    permutation: Update to 0.1.5

--- permutation/src/avx2.rs
+++ permutation/src/avx2.rs
@@ -8,7 +8,7 @@ pub unsafe fn gimli(state: &mut [u32; S], state2: &mut [u32; S]) {
     gimli_x2(state, state2)
 }
 
-#[deprecated(since="0.1.2", note="please use `gimli_x2` instead")]
+#[deprecated(since="0.1.5", note="please use `gimli_x2` instead")]
 #[target_feature(enable = "avx2")]
 pub unsafe fn gimli_x2(state: &mut [u32; S], state2: &mut [u32; S]) {
     simd256::gimli_x2(state, state2)
--- permutation/src/ssse3.rs
+++ permutation/src/ssse3.rs
@@ -1,7 +1,7 @@
 use crate::{ simd128, S };
 
 
-#[deprecated(since="0.1.2", note="please use `gimli` instead")]
+#[deprecated(since="0.1.5", note="please use `gimli` instead")]
 #[target_feature(enable = "ssse3")]
 pub unsafe fn gimli(state: &mut [u32; S]) {
     simd128::gimli(state)
