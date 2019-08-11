02071e182e44ba95be7156d3a00d0ed9cd495ce6
quininer <quininer@live.com>
2019-08-11 23:54:16 +0800

    aead: Add bench

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Gimli: a cross-platform permutation
 
 * [Gimli website](https://gimli.cr.yp.to/index.html)
 * [Reference implementations of the GIMLI permutation](https://github.com/jedisct1/gimli)
+* [Lightweight Cryptography - Round 1 Candidates - Gimli](https://csrc.nist.gov/projects/lightweight-cryptography/round-1-candidates)
---
+++ aead/benches/bench.rs
@@ -0,0 +1,78 @@
+use criterion::{ criterion_main, criterion_group, Criterion, black_box };
+use ring::aead::{ CHACHA20_POLY1305, AES_256_GCM, UnboundKey, Nonce, Aad, LessSafeKey };
+use gimli_aead::GimliAead;
+
+
+fn bench_gimli(c: &mut Criterion) {
+    c.bench_function("gimli", |b| {
+        let key = [0x08; 32];
+        let nonce = [0x09; 16];
+        let buf = vec![0x10; 4096];
+
+        b.iter(move || {
+            let key = black_box(key.clone());
+            let nonce = black_box(nonce.clone());
+            let mut buf = black_box(buf.clone());
+            let mut tag = [0; 16];
+
+            GimliAead::new(&key, &nonce)
+                .encrypt(&[])
+                .process(&mut buf, &mut tag);
+
+            black_box(buf);
+            black_box(tag);
+        });
+    });
+}
+
+
+fn bench_aesgcm(c: &mut Criterion) {
+    c.bench_function("aesgcm", |b| {
+        let key = [0x011; 32];
+        let nonce = [0x12u8; 12];
+        let buf = vec![0x13; 4096];
+
+        b.iter(move || {
+            let key = black_box(key.clone());
+            let nonce = black_box(nonce.clone());
+            let mut buf = black_box(buf.clone());
+
+            let key = UnboundKey::new(&AES_256_GCM, &key).unwrap();
+            let nonce = Nonce::assume_unique_for_key(nonce);
+
+            let tag = LessSafeKey::new(key)
+                .seal_in_place_separate_tag(nonce, Aad::empty(), &mut buf)
+                .unwrap();
+
+            black_box(buf);
+            let _ = black_box(tag);
+        });
+    });
+}
+
+fn bench_chacha20(c: &mut Criterion) {
+    c.bench_function("chacha20", |b| {
+        let key = [0x014; 32];
+        let nonce = [0x15u8; 12];
+        let buf = vec![0x16; 4096];
+
+        b.iter(move || {
+            let key = black_box(key.clone());
+            let nonce = black_box(nonce.clone());
+            let mut buf = black_box(buf.clone());
+
+            let key = UnboundKey::new(&CHACHA20_POLY1305, &key).unwrap();
+            let nonce = Nonce::assume_unique_for_key(nonce);
+
+            let tag = LessSafeKey::new(key)
+                .seal_in_place_separate_tag(nonce, Aad::empty(), &mut buf)
+                .unwrap();
+
+            black_box(buf);
+            let _ = black_box(tag);
+        });
+    });
+}
+
+criterion_group!(aead, bench_aesgcm, bench_chacha20, bench_gimli);
+criterion_main!(aead);
